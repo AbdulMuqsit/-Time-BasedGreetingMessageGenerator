2b2b9178287144d245df10ce1065f2cccd96781a
AbdulMuqsit <abdulmuqsitzargar01@gmail.com>
2023-09-01 21:20:52 +0530

    First commit

---
+++ TimeStamp.py
@@ -0,0 +1,12 @@
+import time
+t = time.strftime('%H:%M:%S')
+hour = int(time.strftime('%H'))
+
+if(0 <= hour < 12):
+    print("Good Morning Sir!")
+elif(12 <= hour < 17):
+    print("Good Afternoon Sir!")
+else:
+    print("Good Night Sir!")
+
+# basic code to gets familarised with the python syntax
